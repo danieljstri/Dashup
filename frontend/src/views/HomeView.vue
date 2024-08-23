78f423720cd7d8c81dc3e3f5306fe73ead25500f
Felipe Quentino <felipequentino22@gmail.com>
2024-08-23 15:06:16 -0300

    :sparkles: feat: added a chart demo with d3.js

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import ItemList from '../components/ItemList.vue';
+import BarChart from '../components/BarChart.vue';
 </script>
 
 <template>
   <main>
+    <BarChart />
     <ItemList />
   </main>
 </template>
