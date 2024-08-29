ab703b9191aa371022e368ccc2603b81d9b774e6
Felipe Quentino <felipequentino22@gmail.com>
2024-08-29 16:56:15 -0300

    :sparkles: feat: added chart page in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/chart">Chart</RouterLink>
       </nav>
     </div>
   </header>
