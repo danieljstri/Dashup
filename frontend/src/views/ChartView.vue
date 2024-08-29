878218dc244c5571bf2352fd0e5246bf8895fb28
Felipe Quentino <felipequentino22@gmail.com>
2024-08-29 16:55:09 -0300

    :sparkles: feat: added view page to visualize all charts

---
+++ frontend/src/views/ChartView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ChartPieComponent from '@/components/charts/ChartPieComponent.vue';
+import ChartComponent from '@/components/charts/ChartComponent.vue';
+</script>
+
+<template>
+    <div class="chart">
+      <h1>Chart Page</h1>
+        <ChartComponent />
+        <ChartPieComponent />
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
