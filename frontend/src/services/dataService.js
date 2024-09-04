b297a2f203b06c438fdc05963c3d4cd84f88f645
Felipe Quentino <felipequentino22@gmail.com>
2024-09-04 16:24:20 -0300

    :bricks: ci: added functions to get the data from the flask backend

--- frontend/src/services/dataService.js
+++ frontend/src/services/dataService.js
@@ -1,13 +1,43 @@
-// services/dataService.js
-
 import axios from 'axios';
 
-export const getData = async () => {
+
+export const getAllData = async () => {
   try {
-    const response = await axios.get('http://localhost:3000/api/items');
+    const response = await axios.get('http://localhost:3000/api/all');
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
   }
 };
+
+export const getProfitData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/lucros/${month}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const getRevenueData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/receitas/${month}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export const getExpensesData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/despesas/${month}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
