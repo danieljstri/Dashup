38dc08d387c83a53ec8f444ee99298a1c3c94194
Felipe Quentino <felipequentino22@gmail.com>
2024-09-05 15:28:38 -0300

    :sparkles: feat: linked the flask routes to the dataService

--- frontend/src/services/dataService.js
+++ frontend/src/services/dataService.js
@@ -31,10 +31,59 @@ export const getRevenueData = async (month="total") => {
   }
 }
 
+export const getRevenueExamsData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/receitas/exames/${month}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export const getRevenueAnesthesiaData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/receitas/anestesia/${month}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export const getRevenueCashData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/receitas/dinheiro/${month}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
 export const getExpensesData = async (month="total") => {
   try {
     const response = await axios.get(`http://localhost:3000/api/despesas/${month}`);
-    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export const getExpensesRentData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/despesas/aluguel/${month}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export const getExpensesAnesthesiaData = async (month="total") => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/despesas/anestesia/${month}`);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
