a184438960810995daf49a930495d7ca8ce8419a
paula argôlo <paulavanessa010@gmail.com>
2024-09-14 22:41:36 -0300

    act:add graphic doghnut to chartPie and inserted conf.ts

---
+++ frontend/src/components/charts/chartConfig.ts
@@ -0,0 +1,14 @@
+export const data = {
+    labels: ['Teste0', 'Teste1', 'Teste2', 'Teste3'],
+    datasets: [
+      {
+        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+        data: [40, 20, 80, 10]
+      }
+    ]
+  }
+  
+  export const options = {
+    responsive: true,
+  }
+  
