316162c85a82a9ffa65daf3c9d250123e2a7ead0
daniel <euft6y@gmail.com>
2024-09-28 15:45:35 -0300

    alt: changed the profit card to contain all the data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,9 @@
-import './assets/main.css'
+
 import 'primeicons/primeicons.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 
