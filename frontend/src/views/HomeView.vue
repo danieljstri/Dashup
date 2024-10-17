cc057a7176abb470f038ea34beb80f31543a2c63
paula argôlo <paulavanessa010@gmail.com>
2024-10-17 12:19:40 -0300

    act: alteração no design da Header superior

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -9,18 +9,21 @@ import ProfitChart from '@/components/general/GeneralChart.vue'
 <template>
   <main>
     <header class="header">
-      <h2>Bem vindo ao DashUp!</h2>
+      <h3>Bem vindo!</h3>
+      <h4>Aqui está um resumo da saúde financeira do seu empreendimento.</h4>
     </header>
+    <div class="txt"><h2>Visão Geral</h2></div>
     <section class="content">
-      <div class="charts">
-       <ProfitChart id="profitchart"/>
-       <ComparisionRxEChart id="comparisionchart"/>
-      </div>
       <div class="cards">
         <ProfitCard/>
         <ExpensesCard/>
         <RevenueCard/>
       </div>
+      <div class="charts">
+       <ProfitChart id="profitchart"/>
+       <ComparisionRxEChart id="comparisionchart"/>
+      </div>
+      
     </section>
   </main>
 </template>
@@ -35,15 +38,43 @@ body{
   height: 100vh;
   width: 100vw;
 }
-h2 {
-  text-align: center;
+
+h3 {
+  margin-top: 15px;
+  margin-left: var(--sidebar-width);
   color: #2f3b36;
+  font-family: 'Noto Sans', sans-serif;
+  font-weight: 600;
 }
+h4 {
+  margin-left: var(--sidebar-width);
+  color: #2f3b36;
+  font-family: 'Noto Sans', sans-serif;
+  font-weight: 400;
+}
+
 header {
-  background-color: #abb5bd;
-  padding: 16px;
-  border-bottom: 2px solid #2f3b36;
+  background-color: #eef6ee;
+  padding: 37px; 
+  border-bottom: 1.5px solid #a8aca776;
+  width: 100%; 
+  box-sizing: border-box; 
+  position: fixed;
+  top: 0;
+  left: 0;
 }
+
+.txt {
+  h2 {
+    margin: 20px 0; 
+    text-align: left; 
+    padding-top: 100px;
+    padding-left: 80px; 
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 300;
+  }
+}
+
 #profitchart, #comparisionchart {
   height: fit-content;
 }
@@ -53,14 +84,15 @@ header {
   padding-bottom: 35px;
   padding-left: 15px;
 }
+
 .content {
   display: flex;
   flex-direction: column;
-  
   align-items: center;
   justify-content: space-between;
-
+  padding-top: 70px; /* Espaço para o cabeçalho fixo */
 }
+
 .charts {
   display: flex;
   justify-content: space-evenly;
