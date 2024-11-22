04a0afb2861549337f739a78643fb7cf63e64150
panyfmc <paulavanessa010@gmail.com>
2024-11-21 23:44:49 -0300

    :white_check_mark: test: standardization of the variable for margin

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script>
   import { ref } from 'vue';
-  import ProfitCard from '@/components/general/ProfitCard.vue';
-  import ExpensesCard from '@/components/general/ExpensesCard.vue';
-  import RevenueCard from '@/components/general/RevenueCard.vue';
-  import ProfitChart from '@/components/general/GeneralChart.vue';
-  import EconomicChart from '@/components/general/EconomicChart.vue';
-  import ValuableProductCard from '@/components/general/Valuable-ProductCard.vue';
-  import EconomicBar from '@/components/general/EconomicBar.vue';
+  import ProfitCard from '@/components/home_page/ProfitCard.vue';
+  import ExpensesCard from '@/components/home_page/ExpensesCard.vue';
+  import RevenueCard from '@/components/home_page/RevenueCard.vue';
+  import ProfitChart from '@/components/home_page/GeneralChart.vue';
+  import EconomicChart from '@/components/home_page/EconomicChart.vue';
+  import ValuableProductCard from '@/components/home_page/Valuable-ProductCard.vue';
+  import EconomicBar from '@/components/home_page/EconomicBar.vue';
 
   const months = [
     { month: "Janeiro / 2024", value: "janeiro" },
@@ -22,34 +22,8 @@
     { month: "Novembro / 2024", value: "novembro" },
     { month: "Dezembro / 2024", value: "dezembro" },
   ];
-  const months = [
-    { month: "Janeiro / 2024", value: "janeiro" },
-    { month: "Fevereiro / 2024", value: "fevereiro" },
-    { month: "Março / 2024", value: "março" },
-    { month: "Abril / 2024", value: "abril" },
-    { month: "Maio / 2024", value: "maio" },
-    { month: "Junho / 2024", value: "junho" },
-    { month: "Julho / 2024", value: "julho" },
-    { month: "Agosto / 2024", value: "agosto" },
-    { month: "Setembro / 2024", value: "setembro" },
-    { month: "Outubro / 2024", value: "outubro" },
-    { month: "Novembro / 2024", value: "novembro" },
-    { month: "Dezembro / 2024", value: "dezembro" },
-  ];
 
 
-  export default {
-    components: {
-      ProfitCard,
-      ExpensesCard,
-      RevenueCard,
-      ProfitChart,
-      EconomicChart,
-      ValuableProductCard,
-      EconomicBar,
-    },
-    setup() {
-      const selectedMonth = ref('janeiro');
   export default {
     components: {
       ProfitCard,
@@ -79,35 +53,8 @@
   <main>
     <section class="header">
       <div>
-  <main>
-    <section class="header">
-      <div>
         <h3>Bem vindo!</h3>
         <h4>Aqui está um resumo da saúde financeira do seu empreendimento.</h4>
-      </div>
-      <select v-model="selectedMonth" id="month-selector">
-        <option v-for="month in months" :value="month.value">
-          {{ month.month }}
-        </option>
-      </select>
-    </section>
-    <!-- Month Selector -->
-    <content class="body-content">
-      <section class="cash-data">
-        <div class="cards">
-          <ProfitCard :selectedMonth="selectedMonth"/>
-          <ExpensesCard :selectedMonth="selectedMonth"/>
-          <RevenueCard :selectedMonth="selectedMonth"/>
-        </div>
-        <ProfitChart :selectedMonth="selectedMonth"/>
-      </section>
-      <section class="control-data">
-        <EconomicChart :selectedMonth="selectedMonth" id="economic-chart"/>
-        <ValuableProductCard :selectedMonth="selectedMonth"/>
-      </section>
-      <EconomicBar />
-    </content>
-  </main>
       </div>
       <select v-model="selectedMonth" id="month-selector">
         <option v-for="month in months" :value="month.value">
@@ -135,88 +82,86 @@
 </template>
 
 <style>
-@import url('https://fonts.cdnfonts.com/css/chillax');
-* { 
-  padding: 0;
-  margin: 0;
-  
-  
-}
+  @import url('https://fonts.cdnfonts.com/css/chillax');
+  * { 
+    padding: 0;
+    margin: 0;
+    
+  }
 
-body {
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-.header {
-  display: flex;
-  width: 100%; 
-  margin-bottom: 1%;
-  box-sizing: border-box;
-  text-align: left;
-  justify-content: space-between; 
-}
-.header h3 {
-  margin-top: 15px;
-  color: #2f3b36;
-  font-weight: 600;
-}
-.header h4 {
-  color: #2f3b36;
-  font-weight: 400;
-}
-#month-selector {
-  margin-top: 15px;
-  padding: 5px;
-  font-size: 16px;
-  font-family: 'Chillax', sans-serif;
-  font-weight: 500;
-  border-radius: 8px;
-  border: 1px solid #CCDEE7;
-  background-color: #ffffff;
-  color:  #245368;
-}
-
-.body-content {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-.cash-data {
-    display: block;
-    gap: 16px;
-    max-width: fit-content;
-}
-.cards {
+  body {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .header {
     display: flex;
-    gap: 16px;
-    margin-bottom: 16px;
-    justify-content: center;
-}
+    width: 100%; 
+    margin-bottom: 1%;
+    box-sizing: border-box;
+    text-align: left;
+    justify-content: space-between; 
+  }
+  .header h3 {
+    margin-top: 15px;
+    color: #2f3b36;
+    font-weight: 600;
+  }
+  .header h4 {
+    color: #2f3b36;
+    font-weight: 400;
+  }
+  #month-selector {
+    margin-top: 15px;
+    padding: 5px;
+    font-size: 16px;
+    font-family: 'Chillax', sans-serif;
+    font-weight: 500;
+    border-radius: 8px;
+    border: 1px solid #CCDEE7;
+    background-color: #ffffff;
+    color:  #245368;
+  }
 
-.control-data {
-    display: flex;
-    flex-direction: column;
-    width: fit-content;
-    height: fit-content;
-    gap: 16px;
-}
-
-
-@media (max-width: 768px) {
   .body-content {
-    flex-direction: column;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 16px;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
   }
 
-  .content {
-    margin-left: var(--sidebar-width-collapsed, 80px); /* Considerar apenas o espaço colapsado */
+  .cash-data {
+      display: block;
+      gap: 16px;
+      max-width: fit-content;
   }
-}
+  .cards {
+      display: flex;
+      gap: 16px;
+      margin-bottom: 16px;
+      justify-content: center;
+  }
 
+  .control-data {
+      display: flex;
+      flex-direction: column;
+      width: fit-content;
+      height: fit-content;
+      gap: 16px;
+  }
+
+
+  @media (max-width: 768px) {
+    .body-content {
+      flex-direction: column;
+    }
+
+    .content {
+      margin-left: var(--sidebar-width); /* Considerar apenas o espaço colapsado */
+    }
+  }
 
 </style>
