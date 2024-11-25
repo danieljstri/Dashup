88f5bcbaceb1bedce2dc1312df3584b208843484
panyfmc <paulavanessa010@gmail.com>
2024-11-24 22:25:40 -0300

    :lipstick: feat: registration screen css styling

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -7,12 +7,20 @@
     signInWithPopup,
   } from 'firebase/auth';
   import { useRouter } from 'vue-router';
+  import loginImagem from "../../../assets/loginImagem.png";
+  import googleLogo from "../../../assets/google-logo.png";
   
   // Variables that will store the email and password
   const email = ref('');
   const password = ref('');
   const errMsg = ref('');
   const router = useRouter();
+  const showPassword = ref(false); // Estado para alternar visibilidade da senha
+
+  // Function to toggle password visibility
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
   
   // Function to create a new user
   const register = () => {
@@ -55,14 +63,380 @@
 </script>
 
 <template>
-    <h1>Criar uma conta</h1>
-    <p><input type="text" placeholder="Email" v-model="email" /></p>
-    <p><input type="password" placeholder="Senha" v-model="password" /></p>
-    <p><button @click="register">Criar conta</button></p>
-    <p><button @click="signInwithGoogle">Registrar com Google</button></p>
-    <p v-if="errMsg">{{ errMsg }}</p>
-    <p>Já tem uma conta? <router-link to="/SignIn">Entrar</router-link></p>
+  <div class="form-container">
+
+    <div class="imgLogin">
+      <img :src="loginImagem" alt="Apresentação" />
+    </div>
+
+    <form class="loginForm" @submit.prevent="register">
+    
+      <div class="textoLogin">
+        <span class="text">Crie sua conta</span>
+        <span class="subtext">Otimize sua gestão e economize mais</span>
+      </div>
+
+      <div class="container">
+
+        <label for="email"><b>Email</b></label>
+        <input
+          id="email"
+          type="email"
+          v-model="email"
+          placeholder="Digite seu email"
+          required
+        />
+
+        <label for="password"><b>Senha</b></label>
+        <div class="input-wrapper">
+          <input
+            id="password"
+            :type="showPassword ? 'text' : 'password'"
+            v-model="password"
+            placeholder="Digite sua senha"
+            required
+          />
+          <button
+            type="button"
+            class="toggle-password"
+            @click="togglePasswordVisibility"
+          >
+          <span class="material-icons">
+            {{ showPassword ? 'visibility' : 'visibility_off' }}
+          </span>
+          </button>
+        </div>
+
+        <div v-if="errMsg" class="error-message">
+          {{ errMsg }}
+        </div>
+
+        <div class="buttons">
+          <button type="submit" class="btn-login">Criar conta</button>
+          <span class="optionLogin">Ou entre com:</span>
+          <button
+            type="button"
+            class="btn-google"
+            @click="signInwithGoogle"
+          >
+            <div class="google-content">
+              <div class="google-icon">
+                <img :src="googleLogo" alt="Google logo" />
+              </div>
+              <span class="google-text">Entre com o Google</span>
+            </div>
+          </button>
+        </div>
+        <p class="account-text">
+          Já tem conta? 
+          <router-link to="/SignIn" class="register-link">Faça o login</router-link>
+        </p>
+      </div>
+    </form>
+  </div>
 </template>
+
+<style scoped>
+
+/* Main container styling */
+.form-container {
+  display: flex;
+  justify-content: center; 
+  align-items: center;    
+  height: 100vh;          
+  max-width: 100%;                
+  box-sizing: border-box; 
+}
+
+.imgLogin {
+  display: none; 
+}
+
+.loginForm {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 500px; 
+  padding: 40px;
+  background: linear-gradient(180deg, rgba(221, 232, 238, 0.5) 48.5%, rgba(222, 230, 234, 0.5) 100%);
+  border-radius: 16px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); 
+}
+
+@media screen and (min-width: 768px) {
+  .form-container {
+    flex-direction: row; 
+    gap: 5rem;         
+  }
+
+  .imgLogin {
+    display: block; 
+    width: 40%;     
+  }
+
+  /* Stylizing the image to occupy the left side */
+  .imgLogin img {
+    max-width: 100%; 
+    height: auto;    
+  }
+
+  /* Styling the Login form  */
+  .loginForm {
+    width: 60%; 
+    padding: 40px;
+  }
+}
+
+.textoLogin {
+  display: flex;
+  flex-direction: column; 
+  align-items: center;       
+}
+
+/* Login text styling */
+.text {
+  font-family: 'General Sans Variable', sans-serif;
+  font-size: 36px;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+  text-align: center;
+  text-decoration-skip-ink: none;
+  color: #245368;
+}
+
+
+/* Stylization of the subtext accompanying the Login */
+.subtext {
+  color: #245368; 
+  font-size: 18px; 
+  line-height: 25.2px;
+  font-family: 'Chillax', sans-serif;
+  font-weight: 400;
+  letter-spacing: 0.05em;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+/* Styling Email and Password texts */
+.container label {
+  display: block;
+  padding: 1.7rem 0 0.5rem 0.5rem;
+  font-family: 'General Sans Variable', sans-serif;
+}
+
+label {
+  font-family: 'General Sans Variable', sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 21.6px;
+  letter-spacing: 0.07em;
+  color: #538094;
+
+}
+
+/* Style for placeholder text */
+input::placeholder {
+  font-family: 'General Sans Variable', sans-serif;
+  font-size: 16px; 
+  color: #245368; 
+  font-weight: 500; 
+  letter-spacing: 0.08em; 
+  line-height: 21.6px; 
+}
+
+/* text inside the box */
+input:required {
+  font-size: 16px;
+  color: #334f5b;
+  font-weight: 500;
+  letter-spacing: 0.08em;
+  font-family: 'General Sans Variable', sans-serif;
+}
+
+.input-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+input {
+  width: 100%;
+}
+
+.toggle-password {
+  position: absolute;
+  right: 10px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 1.2em;
+}
+
+.material-icons {
+  vertical-align: middle;
+  color: #538094;
+}
+
+/* Boder box Email and password */
+.container input[type="email"],
+.container input[type="password"],
+.container input[type="text"] {
+  width: 100%;
+  height: 52.1px;
+  padding: 15px 20px;
+  display: inline-block;
+  border: 1px solid #ccdee7; 
+  border-radius: 16px;
+  box-sizing: border-box;
+  background: #ffffff;
+}
+
+.error-message {
+  font-family: 'General Sans Variable', sans-serif;
+  font-size: 16px;
+  color: #538094;
+  font-weight: 500; 
+  letter-spacing: 0.05em; 
+  padding-top: 1.5rem;
+  text-align: center;
+  display: block;
+  background: none;
+  border: none;
+  color: #538094;
+  cursor: pointer;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem 0 1.5rem 0;
+}
+
+.btn-login {
+  width: 461px; 
+  height: 55px; 
+  padding: 10px;
+  border-radius: 16px;
+  border: 1px solid #ccdee7;
+  background-color: #245368; 
+  color: #fff; 
+  font-family: 'General Sans Variable', sans-serif; 
+  font-weight: 500; 
+  font-size: 16px; 
+  text-align: center; 
+  line-height: 21px; 
+  letter-spacing: 0.09em;
+  cursor: pointer; 
+  transition: background-color 0.3s ease; 
+}
+
+.btn-login:hover {
+  background-color: #1b4253d9;
+  opacity: 1;
+}
+
+/* Login option styling */
+.optionLogin {
+  font-family: 'Chillax', sans-serif;
+  font-size: 16px;
+  color: #538094;
+  font-weight: 400; 
+  letter-spacing: 0.05em; 
+  line-height: 21.6px;
+  text-align: center;
+  display: block; /* Ensures that the element respects the spacing */
+  padding: 20px;
+}
+
+/* Styling the Login with Google option */
+.google-content {
+  display: flex; 
+  align-items: center; 
+  justify-content: center; 
+  width: 461px;
+  height: 53px;
+  border-radius: 16px;
+  border: 1px solid #ccdee7;
+  background-color: #ffffff; 
+  font-family: 'General Sans Variable', sans-serif;
+  cursor: pointer;
+  white-space: nowrap;
+  font-weight: 600;
+}
+
+/* Adjustments to the logo icon */
+.google-icon img {
+  width: 24px;
+  height: 24px;
+  margin-right: 10px;
+}
+
+/* Button text adjustments */
+.google-text {
+  line-height: 1; /* Align the text with the icon */
+  color: #111216; 
+  font-family: 'General Sans Variable', sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 22.4px;
+  letter-spacing: 0.05em;
+  text-align: left;
+  text-decoration-skip-ink: none;
+
+}
+
+.google-content:hover {
+  background-color: #f4fef4d1;
+  transition: all 0.2s ease-in-out;
+  opacity: 1;
+}
+
+/* Estilo para o texto "Já tem conta?" */
+.account-text {
+  font-family: 'Chillax', sans-serif;
+  font-size: 14px;
+  color: #245368;
+  font-weight: 400; 
+  letter-spacing: 0.03em; 
+  line-height: 19.6px;
+  text-align: center;
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  gap: 0.5rem; 
+}
+
+/* Estilo para o link "Faça o login" */
+.register-link {
+  font-family: 'General Sans Variable', sans-serif;
+  color: #245368;
+  font-size: 14px;
+  font-weight: 400; 
+  letter-spacing: 0.03em; 
+  line-height: 19.6px;
+  text-decoration: underline solid;
+  text-align: center;
+  text-underline-offset: 19%;
+  text-decoration-thickness: 7.5%;
+  text-decoration-skip-ink: auto;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+    display: block;
+    float: none;
+  }
+  .cancelbtn {
+    width: 100%;
+  }
+}
+</style>
+
   
   
 
